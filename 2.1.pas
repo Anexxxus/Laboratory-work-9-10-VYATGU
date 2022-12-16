d9f0b9a964b7f25aff6605c2123c694f0bfff4de
Konstantin Borisov <68741206+Anexxxus@users.noreply.github.com>
2022-12-16 22:54:59 +0300

    Add files via upload

---
+++ 2.1.pas
@@ -0,0 +1,11 @@
+﻿begin
+  var b := new integer[6];
+  var a := MatrRandomInteger(6, 8, 0, 10);
+  a.println;
+  println();
+  for var i := 0 to a.rowcount - 1 do 
+  begin
+    if abs(a[i, 0]) > 4 then b[i] := a[i, 0];
+  end;
+  b.println;
+end.
---
+++ 2.2.pas
@@ -0,0 +1,19 @@
+﻿const m = 8;
+  n = 6;
+begin
+  var b := new integer[m];
+  var a := MatrRandomInteger(m, n, -1, 10);
+  a.println;
+  println();
+  var s := 0;
+  for var i := 0 to a.rowcount - 1 do
+  begin
+    for var j := 0 to a.colcount - 1 do
+    begin
+      if a[i, j] < 0 then inc(s);
+      if s <> 0 then b[i] := -1 else b[i] := 1;
+    end;
+    s := 0;
+  end;
+  b.println;
+end.
